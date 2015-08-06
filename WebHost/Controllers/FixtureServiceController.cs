d2e41e0ddc3f990ea69ae03ef96d3f4f59b2730c
Ewan <echilton@sportingsolutions.com>
2015-08-06 16:41:06 +0100

    updated webhost

--- WebHost/Controllers/FixtureServiceController.cs
+++ WebHost/Controllers/FixtureServiceController.cs
@@ -49,6 +49,20 @@ namespace WebHost.Controllers
             return this.fixtureService.AddFixture(fixture);
         }
 
+        [HttpGet]
+        [HttpPost]
+        public List<Fixture> GetFixtureByDate(DateTime startDate, DateTime endDate)
+        {
+            //todo::check the auth key!
+            //log the version number
+            logger.Info(
+                "received message from version : " + GetVersion() +
+                " from authKey " + GetAuthKey() +
+                " GetFixtureByDate : " + startDate.ToShortDateString() + " - " + endDate.ToShortDateString());
+
+            return this.fixtureService.GetFixtureByDate(startDate, endDate);
+        }
+
         private string GetVersion()
         {
             string versionString = "not specified";
